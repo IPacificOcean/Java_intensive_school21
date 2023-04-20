67c050d9cf6aee4276574d0806e08e4cba118e72
qyburnbo <qyburnbo@student.21-school.ru>
2023-04-20 20:57:56 +0300

    ex04 input

--- src/ex04/Program.java
+++ src/ex04/Program.java
@@ -1,16 +1,27 @@
 package ex04;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-//        String str = "ABCAJHNXATGDBDGJSAMJHCGDMJH";
-        String output;
+//        String str = "ABCAJHNXATGDBDGJSAMJHCGDMJH"; SHello, worlds!
 
-        String str = "Hello, world!";
-        int[] charCount = new int[256]; // 256 is the maximum possible value for a char
+        Scanner scaner = new Scanner(System.in);
+        String str = scaner.nextLine();
+        System.out.println(str);
+        scaner.close();
+
+        char[] input = str.toCharArray();
+        int[] charCount = new int[65535];
+        char[] whatIsSimbol = new char[65535];
+        int index = 0;
         for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
+            char c = input[i];
+            if (charCount[c] == 0) {
+                whatIsSimbol[index] = c;
+                ++index;
+            }
             charCount[c]++;
         }
 
@@ -19,5 +30,12 @@ public class Program {
                 System.out.println((char) i + ": " + charCount[i]);
             }
         }
+        for (int i = 0; whatIsSimbol[i] != 0; i++) {
+            System.out.print(whatIsSimbol[i] + " ");
+        }
+    }
+    static void sort(int[] input) {
     }
 }
+
+
