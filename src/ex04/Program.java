ee7f3bb3699c43c2ec4d6998feaaf9909072140d
qyburnbo <qyburnbo@student.21-school.ru>
2023-04-21 01:19:43 +0300

    ex04 sort1, print

--- src/ex04/Program.java
+++ src/ex04/Program.java
@@ -1,6 +1,5 @@
 package ex04;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Program {
@@ -9,43 +8,47 @@ public class Program {
 
         Scanner scaner = new Scanner(System.in);
         String str = scaner.nextLine();
-        System.out.println(str);
         scaner.close();
 
         char[] input = str.toCharArray();
         int[] charCount = new int[65535];
-        char[] whatIsSimbol = new char[65535];
+        char[] whatIsSymbol = new char[65535];
         int index = 0;
         for (int i = 0; i < str.length(); i++) {
             char c = input[i];
             if (charCount[c] == 0) {
-                whatIsSimbol[index] = c;
+                whatIsSymbol[index] = c;
                 ++index;
             }
             charCount[c]++;
         }
-        int length = getLenOnlyArrayOfCharacters(whatIsSimbol);
-        sort(whatIsSimbol, charCount, length, true);
-        sort(whatIsSimbol, charCount, length, false);
+        int length = getOnlyLenArrayOfCharacters(whatIsSymbol);
+        sort(whatIsSymbol, charCount, length, true);
+        sort(whatIsSymbol, charCount, length, false);
 
         //Print//
-        for (int i = 0; i < charCount.length; i++) {
-            if (charCount[i] > 0) {
-                System.out.println((char) i + ": " + charCount[i]);
+
+        for (int i = 0; i < 10; ++i){
+            if (whatIsSymbol[i] == 0) {
+                break;
             }
+            System.out.print(charCount[whatIsSymbol[i]] + " ");
         }
-        for (int i = 0; whatIsSimbol[i] != 0; i++) {
-            System.out.print(whatIsSimbol[i] + " ");
+            System.out.println();
+
+
+        for (int i = 0; whatIsSymbol[i] != 0; i++) {
+            System.out.print(whatIsSymbol[i] + " ");
         }
     }
 
+
     static void sort(char[] input, int[] charCount, int length, boolean lex_sort) {
-        boolean swapped = false;
         for (int i = 0; i < length - 1; ++i) {
-            swapped = false;
+            boolean swapped = false;
             for (int j = 0; j < length - i - 1; ++j) {
                 if (lex_sort) {
-                    if (input[j] >input[j + 1]) {
+                    if (input[j] > input[j + 1]) {
                         swapped = swap(input, j);
                     }
                 } else {
@@ -60,43 +63,14 @@ public class Program {
         }
     }
 
-    static void sortByCount(char[] input, int[] charCount, int length) {
-        boolean swapped = false;
-        for (int i = 0; i < length - 1; ++i) {
-            swapped = false;
-            for (int j = 0; j < length - i - 1; ++j) {
-                    if (charCount[input[j]] < charCount[input[j + 1]]) {
-                        swapped = swap(input, j);
-                    }
-            }
-            if (!swapped) {
-                break;
-            }
-        }
-    }
-    static void lexsSort(char[] input, int length) {
-        boolean swapped = false;
-        for (int i = 0; i < length - 1; ++i) {
-            swapped = false;
-            for (int j = 0; j < length - i - 1; ++j) {
-                    if (input[j] >input[j + 1]) {
-                        swapped = swap(input, j);
-                    }
-            }
-            if (!swapped) {
-                break;
-            }
-        }
-    }
-
     static boolean swap(char[] input, int j) {
         char temp = input[j];
         input[j] = input[j + 1];
         input[j + 1] = temp;
-        return  true;
+        return true;
     }
 
-    static int getLenOnlyArrayOfCharacters(char[] input) {
+    static int getOnlyLenArrayOfCharacters(char[] input) {
         int length = 0;
         for (char c : input) {
             if (c == 0) {
