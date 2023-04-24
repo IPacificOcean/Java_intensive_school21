d68518d7cceb077a858f9fe685dcb7bbae8ef04e
qyburnbo <qyburnbo@student.21-school.ru>
2023-04-24 19:15:42 +0300

    user, transaction

--- src/day01/ex00/User.java
+++ src/day01/ex00/User.java
@@ -6,4 +6,36 @@ public class User {
    private int uid;
    private String name;
    private long balance;
+
+   public void setUid(int uid) {
+      if (uid <= 0) {
+         System.err.println("IllegalArgument uid");
+         System.exit(-1);
+      }
+         this.uid = uid;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public void setBalance(long balance) {
+      if (balance < 0) {
+         System.err.println("IllegalArgument balance");
+         System.exit(-1);
+      }
+      this.balance = balance;
+   }
+
+   public int getUid() {
+      return uid;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public long getBalance() {
+      return balance;
+   }
 }
