87242f47bfbc28a2dab3738854fa049f672ffacc
qyburnbo <qyburnbo@student.21-school.ru>
2023-04-25 20:35:59 +0300

    UserList

--- day01/src/ex02/UserList.java
+++ day01/src/ex02/UserList.java
@@ -1,4 +1,9 @@
 package ex02;
 
 public interface UserList {
+    void add(User user);
+    User retrieveUserById (int id);
+    User retrieveUserByIndex (int index);
+    User retrieveNumberOfUsers (int num);
+
 }
