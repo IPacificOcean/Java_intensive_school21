f681880db79a63d436ab33e2dc0b45607ff81c2c
qyburnbo <qyburnbo@student.21-school.ru>
2023-05-13 19:26:51 +0300

    day03 exO2 Refact. decompos.

--- day03/src/ex02/Summator.java
+++ day03/src/ex02/Summator.java
@@ -8,11 +8,12 @@ import java.util.concurrent.*;
 public class Summator {
 
 
+    private int first_;
+    private int last_;
+    private final List<Future<Integer>> intermediateAmounts_ = new ArrayList<>();
+
     int SumByThread(int[] arr, int threadCount_) throws ExecutionException, InterruptedException {
         ExecutorService executorService_ = Executors.newFixedThreadPool(threadCount_);
-        List<Future<Integer>> intermediateAmounts_ = new ArrayList<>();
-        int first_;
-        int last_;
         int lastTread = threadCount_ - 1;
         int countItems = arr.length / threadCount_;
         for (int i = 0; i < threadCount_; ++i) {
@@ -22,23 +23,29 @@ public class Summator {
             } else {
                 last_ = countItems + countItems * i;
             }
-
-            int begin = first_;
-            int end = last_;
-            int count = i;
-            intermediateAmounts_.add(executorService_.submit(() -> {
-                int intermedSum = Arrays.stream(arr, begin, end).sum();
-                String threadName = Thread.currentThread().getName();
-                System.out.printf("Thread %d( %s ): from %d to %d sum is %d\n", count + 1, threadName, begin, end, intermedSum);
-                return intermedSum;
-            }));
+            executorThreads(arr, i, executorService_);
         }
         executorService_.shutdown();
+
+        return finalAmount();
+    }
+
+    private void executorThreads(int[] arr, int index, ExecutorService executorService_) {
+        int begin = first_;
+        int end = last_;
+        intermediateAmounts_.add(executorService_.submit(() -> {
+            int intermedSum = Arrays.stream(arr, begin, end).sum();
+            String threadName = Thread.currentThread().getName();
+            System.out.printf("Thread %d( %s ): from %d to %d sum is %d\n", index + 1, threadName, begin, end, intermedSum);
+            return intermedSum;
+        }));
+    }
+
+    private int finalAmount() throws ExecutionException, InterruptedException {
         int amount = 0;
         for (Future<Integer> item : intermediateAmounts_) {
             amount += item.get();
         }
-
         return amount;
     }
 }
