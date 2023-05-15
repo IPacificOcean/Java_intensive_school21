7f9d21ef8dbf655e77bae1ea9b9175041f785e12
qyburnbo <qyburnbo@student.21-school.ru>
2023-05-15 03:01:07 +0300

    day03 exO3 txtToList

--- day03/src/ex03/Downloader.java
+++ day03/src/ex03/Downloader.java
@@ -1,38 +1,57 @@
 package ex03;
 
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Downloader {
+    private static final int BUFFSIZE = 1024;
+    private final String fileOfUrls_ = "day03/src/ex03/files_urls2.txt";
+    private final String pathResult_ = "day03/src/ex03/download/";
 
-    void download() throws IOException {
-        URL fLoad = new URL("https://www.sli.komi.com/files/stipendii/instruction_word_to_pdf.pdf");
-        String fNameUrl = fLoad.getFile();
-        Path p = Paths.get(fNameUrl);
+    private final List<String> listUrls_ = new ArrayList<>();
+
+    void txtToList() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileOfUrls_))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Collections.addAll(listUrls_, line.substring(2));
+            }
+//                for (String s : listUrls_)
+//                System.out.println(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void download(int i) throws IOException {
+        URL fLoad = new URL(listUrls_.get(i));
+        Path p = Paths.get(fLoad.getFile());
         String fName = p.getFileName().toString();
-        String result = "day03/src/ex03/" + fName;
+        String nameResult_ = pathResult_ + fName;
 
         try {
-//            downloadUsingStream(fLoad, result); // Variant N1 (option)
-            downloadUsingNIO(fLoad, result); // Variant N2 (option)
+            downloadUsingStream(fLoad, nameResult_); // Variant N1 (option)
+//            downloadUsingNIO(fLoad, result); // Variant N2 (option)
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
-// Variant N1
+
+    // Variant N1
     private void downloadUsingStream(URL fLoad, String result) throws IOException {
-        BufferedInputStream in = new BufferedInputStream(fLoad.openStream());
-        FileOutputStream fout = new FileOutputStream(result);
-        byte[] buffer = new byte[1024];
+        BufferedInputStream in = new BufferedInputStream(fLoad.openStream(), BUFFSIZE);
+        BufferedOutputStream fout = new BufferedOutputStream(new FileOutputStream(result));
+        byte[] buffer = new byte[BUFFSIZE];
         int count;
-        while ((count = in.read(buffer, 0, 1024)) != -1) {
+        while ((count = in.read(buffer)) != -1) {
             fout.write(buffer, 0, count);
         }
         in.close();
@@ -40,7 +59,7 @@ public class Downloader {
     }
 
     // Variant N2
-   private void downloadUsingNIO(URL fLoad, String result) throws IOException {
+    private void downloadUsingNIO(URL fLoad, String result) throws IOException {
         ReadableByteChannel rbc = Channels.newChannel(fLoad.openStream());
         FileOutputStream fos = new FileOutputStream(result);
         fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
--- day03/src/ex03/Program.java
+++ day03/src/ex03/Program.java
@@ -1,14 +1,20 @@
 package ex03;
 
+import java.util.concurrent.TimeUnit;
+
 public class Program {
-public static void main(String[] args) throws Exception {
-    Validation v = new Validation();
-    v.check(args);
-    int threadCount = v.getThreadCount_();
+    public static void main(String[] args) throws Exception {
+        Validation v = new Validation();
+        v.check(args);
+        int threadCount = v.getThreadCount_();
 
-    Downloader d = new Downloader();
-    d.download();
+        Downloader d = new Downloader();
+        d.txtToList();
+        for (int i = 0; i < 8; ++i) {
+//            TimeUnit.SECONDS.sleep(5);
+            d.download(i);
+        }
 
-}
+    }
 
 }
