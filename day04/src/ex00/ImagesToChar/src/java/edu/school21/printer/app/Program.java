42e7b63e0d60ca975b7f983985c3288dfd096be0
qyburnbo <qyburnbo@student.21-school.ru>
2023-05-16 23:55:10 +0300

    ex00 start

---
+++ day04/src/ex00/ImagesToChar/src/java/edu/school21/printer/app/Program.java
@@ -0,0 +1,7 @@
+package java.edu.school21.printer.app;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ day04/src/ex00/ImagesToChar/src/java/edu/school21/printer/logic/Validation.java
@@ -0,0 +1,46 @@
+package java.edu.school21.printer.logic;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Validation {
+    private String  white_;
+    private String  black_;
+    private String  pathToImg_;
+
+    public String getWhite_() {
+        return white_;
+    }
+
+    public String getBlack_() {
+        return black_;
+    }
+
+    public String getPathToImg_() {
+        return pathToImg_;
+    }
+
+    public void check(String[] args) {
+        if ((args.length >= 3) && args[0].equals(".") && args[1].equals("0")) {
+            try {
+                white_ = args[0];
+                black_ = args[1];
+                Path file = Paths.get(args[2]);
+                if (!Files.exists(file)) {
+                    exit(args[2] + "file does not exist");
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            exit("too few arguments or error of input");
+        }
+    }
+
+    void exit (String error) {
+        System.err.println(error);
+        System.exit(-1);
+    }
+}
