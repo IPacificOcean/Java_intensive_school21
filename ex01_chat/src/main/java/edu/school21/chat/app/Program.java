2824ad06f5185d5d67431959fdce4baa3ed57482
qyburnbo <qyburnbo@student.21-school.ru>
2023-05-23 02:05:00 +0300

    ex01_chat SecureStatements

--- ex01_chat/src/main/java/edu/school21/chat/app/Program.java
+++ ex01_chat/src/main/java/edu/school21/chat/app/Program.java
@@ -4,8 +4,9 @@ package edu.school21.chat.app;
 import edu.school21.chat.repositories.DBWorker;
 import edu.school21.chat.repositories.MessagesRepository;
 import edu.school21.chat.repositories.MessagesRepositoryJdbcImpl;
+import edu.school21.chat.repositories.SecureStatements;
 
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -14,30 +15,29 @@ public class Program {
     public static final String INSERT_BOOK = "insert into book values(?, ?)";
     public static final String GET_ALL = "select * from book order by id desc";
     public static final String DELETE = "delete from book where id = ?";
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         DBWorker dbWorker = new DBWorker();
 
         MessagesRepository mr = new MessagesRepositoryJdbcImpl(dbWorker.getDS());
+        SecureStatements secureStatements = new SecureStatements(dbWorker);
 
-        try (PreparedStatement pStatement = dbWorker.getConnection().prepareStatement(DELETE);){
-//            pStatement.setInt(1, 8);
-//            pStatement.setString(2, "Leskov");
-//            pStatement.execute();
-//            ResultSet res = pStatement.executeQuery();
-//
-//            while (res.next()) {
-//                int id = res.getInt("id");
-//                String author = res.getString("name");
-//                System.out.println("id = " + id + " author: " + author);
-//            }
+        secureStatements.preparedStatement(GET_ALL, (statement) -> {
+//                    statement.setInt(1, 8);
+//                    statement.setString(2, "Leskov");
+//                    statement.execute();
 
-            pStatement.setInt(1, 8);
-            pStatement.executeUpdate();
+//                    statement.setInt(1, 8);
+//                    statement.executeUpdate();
 
+                    ResultSet res = statement.executeQuery();
+                    while (res.next()) {
+                        int id = res.getInt("id");
+                        String author = res.getString("name");
+                        System.out.println("id = " + id + " author: " + author);
+                    }
+                }
+        );
 
-        } catch (SQLException e) {
-            System.out.println("error connection in program");
-        }
         System.out.println("__________________________bay");
     }
 }
--- ex01_chat/src/main/java/edu/school21/chat/repositories/SecureStatements.java
+++ ex01_chat/src/main/java/edu/school21/chat/repositories/SecureStatements.java
@@ -1,4 +1,48 @@
 package edu.school21.chat.repositories;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
 public class SecureStatements {
+    DBWorker connectPool_;
+
+    public SecureStatements(DBWorker connectPool) {
+        connectPool_ = connectPool;
+    }
+
+    @FunctionalInterface
+    public interface SQLConsumer<T> {
+        void accept(T object) throws SQLException;
+    }
+
+    public void connection(SQLConsumer<? super Connection> consumer) throws SQLException {
+        Objects.requireNonNull(consumer);
+        try (Connection connect = connectPool_.getConnection()) {
+            consumer.accept(connect);
+        }
+    }
+
+    public void statement(SQLConsumer<? super Statement> consumer) throws SQLException {
+        Objects.requireNonNull(consumer);
+        connection((connect) -> {
+            try (Statement statement = connect.createStatement()) {
+                consumer.accept(statement);
+            }
+        });
+    }
+
+    public void preparedStatement(String sql, SQLConsumer<? super PreparedStatement> consumer)
+            throws SQLException {
+        Objects.requireNonNull(consumer);
+        connection((connect) -> {
+            try (PreparedStatement pStatement = connect.prepareStatement(sql)) {
+                consumer.accept(pStatement);
+            }
+        });
+    }
+
+
 }
