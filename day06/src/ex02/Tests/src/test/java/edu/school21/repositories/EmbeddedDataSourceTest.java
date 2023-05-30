e4c61f4346dcbb1563aa918e6ea2c922befedb33
qyburnbo <qyburnbo@student.21-school.ru>
2023-05-31 00:48:14 +0300

    ex02 ProductRepositoryJdbcImpl

---
+++ day06/src/ex02/Tests/src/test/java/edu/school21/repositories/EmbeddedDataSourceTest.java
@@ -0,0 +1,28 @@
+package edu.school21.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class EmbeddedDataSourceTest {
+    private DataSource dataSource;
+
+    @BeforeEach
+    public void init() {
+        dataSource = new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScripts("schema.sql", "data.sql")
+                .build();
+    }
+
+    @Test
+    public void test() throws SQLException {
+        assertNotNull(dataSource.getConnection());
+    }
+}
---
+++ day06/src/ex02/Tests/src/test/resources/data.sql
@@ -0,0 +1,6 @@
+insert into tests.products (name, price)
+values ('Pizza', 61.65),
+       ('Cheese', 46.39),
+       ('Macaroons', 12.89),
+       ('Milk', 10.4),
+       ('Cranberries', 81.36);
