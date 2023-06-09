44e8250e672ac690ae0b918deae1441afd4c8323
qyburnbo <qyburnbo@student.21-school.ru>
2023-06-09 03:27:33 +0300

    ex02 ORM CreateTables

---
+++ day07/src/ex02/ORM/src/main/java/edu/school21/repositories/CreateTables.java
@@ -0,0 +1,33 @@
+package edu.school21.repositories;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CreateTables {
+    SecureStatements dbConnect_;
+
+    public CreateTables(DBWorker dbConnect) {
+        dbConnect_ = new SecureStatements(dbConnect.getDS());
+    }
+
+    public void CreateTab(String schema, String data) {
+        try (Stream<String> schemaPath = Files.lines(Paths.get(schema).toAbsolutePath().normalize());
+             Stream<String> dataPath = Files.lines(Paths.get(data).toAbsolutePath().normalize())
+        ) {
+
+            String schemaQwery = schemaPath.collect(Collectors.joining(System.lineSeparator()));
+            String dataQwery = dataPath.collect(Collectors.joining(System.lineSeparator()));
+
+            dbConnect_.statement((statement) -> {
+                statement.execute(schemaQwery);
+                statement.execute(dataQwery);
+            });
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
