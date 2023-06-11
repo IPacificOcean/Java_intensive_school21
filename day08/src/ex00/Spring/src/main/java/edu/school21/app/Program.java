a0a077189067882bbf77a0b6060de25b284c233e
qyburnbo <qyburnbo@student.21-school.ru>
2023-06-11 17:41:13 +0300

    day08 ex00 works tests

--- day08/src/ex00/Spring/src/main/java/edu/school21/app/Program.java
+++ day08/src/ex00/Spring/src/main/java/edu/school21/app/Program.java
@@ -4,7 +4,6 @@ import edu.school21.interfaces.PreProcessor;
 import edu.school21.interfaces.Printer;
 import edu.school21.interfaces.Renderer;
 import edu.school21.service.PreProcessorToUpperImpl;
-import edu.school21.service.PrinterWithDateTimeImpl;
 import edu.school21.service.PrinterWithPrefixImpl;
 import edu.school21.service.RendererErrImpl;
 import org.springframework.context.ApplicationContext;
@@ -16,30 +15,35 @@ public class Program {
         PreProcessor preProcessor = new PreProcessorToUpperImpl();
         Renderer renderer = new RendererErrImpl(preProcessor);
         PrinterWithPrefixImpl printer = new PrinterWithPrefixImpl(renderer);
-        PrinterWithDateTimeImpl printer2 = new PrinterWithDateTimeImpl(renderer);
+//        PrinterWithDateTimeImpl printer2 = new PrinterWithDateTimeImpl(renderer);
         printer.setPrefix("Prefix");
         printer.print("Hello!");
-        printer2.print("Hello!");
+        System.out.println("Without using a spring");
+//        printer2.print("Hello!");
 
         System.out.println("______________________________");
+        System.out.println();
 
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
-        Printer printerWithDateErrToUpper = context.getBean("printerWithDateErrToUpper", Printer.class);
-        printerWithDateErrToUpper.print("Hello!");
-        System.out.println("______________________________");
-
-        Printer printerWithDateStdToUpper = context.getBean("printerWithDateStdToUpper", Printer.class);
-        printerWithDateStdToUpper.print("Hello!");
-
-
-        Printer printerWithPrefixStdToUpper =
-                context.getBean("printerWithPrefixStdToUpper", Printer.class);
-        printerWithPrefixStdToUpper.print("Hello!");
-
         Printer printerWithPrefixErrToUpper =
                 context.getBean("printerWithPrefixErrToUpper", Printer.class);
+        System.out.println("With using a spring");
         printerWithPrefixErrToUpper.print("Hello!");
+
+
+//        Printer printerWithDateErrToUpper = context.getBean("printerWithDateErrToUpper", Printer.class);
+//        printerWithDateErrToUpper.print("Hello!");
+//        System.out.println("______________________________");
+//
+//        Printer printerWithDateStdToUpper = context.getBean("printerWithDateStdToUpper", Printer.class);
+//        printerWithDateStdToUpper.print("Hello!");
+//
+//
+//        Printer printerWithPrefixStdToUpper =
+//                context.getBean("printerWithPrefixStdToUpper", Printer.class);
+//        printerWithPrefixStdToUpper.print("Hello!");
+
 
     }
 }
