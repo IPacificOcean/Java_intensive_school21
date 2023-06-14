f02f22b3605c3c5ae1f838a470e82affee7ca7ef
qyburnbo <qyburnbo@student.21-school.ru>
2023-06-14 23:18:08 +0300

    ex02 pom

--- day08/src/ex02/Service/src/main/java/school21/spring/service/application/Main.java
+++ day08/src/ex02/Service/src/main/java/school21/spring/service/application/Main.java
@@ -7,27 +7,27 @@ import school21.spring.service.IOData.OutputTamplate;
 public class Main {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        OutputStandard outputStandard = new OutputStandard(context);
-        OutputTamplate outputTamplate = new OutputTamplate(context);
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+            OutputStandard outputStandard = new OutputStandard(context);
+            OutputTamplate outputTamplate = new OutputTamplate(context);
 
-        try {
-            System.out.println("_______Standard_________");
-            outputStandard.output();
-            System.out.println();
+            try {
+                System.out.println("_______Standard_________");
+                outputStandard.output();
+                System.out.println();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
 
-        try {
-            System.out.println("_______Template_________");
-            outputTamplate.output();
+            try {
+                System.out.println("_______Template_________");
+                outputTamplate.output();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
     }
 }
--- day08/src/ex02/Service/src/main/java/school21/spring/service/config/ApplicationConfig.java
+++ day08/src/ex02/Service/src/main/java/school21/spring/service/config/ApplicationConfig.java
@@ -1,4 +1,7 @@
 package school21.spring.service.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ApplicationConfig {
 }
